a9285e2f5a458fea761a22d343f669864a62d07f
WinCNT <34294605+WinCNT-SHIM@users.noreply.github.com>
2025-07-10 23:54:07 +0900

    소프트 쉐도우 처리는 Shadows.hlsl에 맡기도록 변경(퀄리티 설정은 아직 적용 안 됨)
    키워드에 대한 설정이 즉각적으로 되는 건 아닌가?

--- Assets/Gaku/Shaders/GakuShadows.hlsl
+++ Assets/Gaku/Shaders/GakuShadows.hlsl
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl"
+
 float GetSelfShadow(float3 positionWS, float3 normalWS)
 {
     float attenuation = 1;
@@ -19,26 +21,30 @@ float GetSelfShadow(float3 positionWS, float3 normalWS)
     #endif
     float4 selfShadowCoord = float4(shadowMapUV_XY, ndcZCompareValue, 0);
 
-    // 셀프 쉐도우 맵을 샘플링
-    #if _SHADOWS_SOFT
+    half4 shadowParams = GetMainLightShadowParams();
     ShadowSamplingData shadowSamplingData = (ShadowSamplingData)0;
     shadowSamplingData.shadowmapSize = _GakuSelfShadowParam;
     shadowSamplingData.shadowOffset0 = float4(-_GakuSelfShadowParam.x, -_GakuSelfShadowParam.y, _GakuSelfShadowParam.x, -_GakuSelfShadowParam.y);
     shadowSamplingData.shadowOffset1 = float4(-_GakuSelfShadowParam.x, _GakuSelfShadowParam.y, _GakuSelfShadowParam.x, _GakuSelfShadowParam.y);
-
-    // Shadows.hlsl의 SampleShadowmapFilteredLowQuality
-    float4 attenuation4;
-    attenuation4.x = float(SAMPLE_TEXTURE2D_SHADOW(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT, selfShadowCoord.xyz + float3(shadowSamplingData.shadowOffset0.xy, 0)));
-    attenuation4.y = float(SAMPLE_TEXTURE2D_SHADOW(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT, selfShadowCoord.xyz + float3(shadowSamplingData.shadowOffset0.zw, 0)));
-    attenuation4.z = float(SAMPLE_TEXTURE2D_SHADOW(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT, selfShadowCoord.xyz + float3(shadowSamplingData.shadowOffset1.xy, 0)));
-    attenuation4.w = float(SAMPLE_TEXTURE2D_SHADOW(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT, selfShadowCoord.xyz + float3(shadowSamplingData.shadowOffset1.zw, 0)));
-    attenuation = dot(attenuation4, float(0.25));
+    
+    // 셀프 쉐도우 맵을 샘플링
+    #if defined(_SHADOWS_SOFT_LOW)
+    attenuation = SampleShadowmapFilteredLowQuality(TEXTURE2D_SHADOW_ARGS(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT), selfShadowCoord, shadowSamplingData);
+    #elif defined(_SHADOWS_SOFT_MEDIUM)
+    attenuation = SampleShadowmapFilteredMediumQuality(TEXTURE2D_SHADOW_ARGS(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT), selfShadowCoord, shadowSamplingData);
+    #elif defined(_SHADOWS_SOFT_HIGH)
+    attenuation = SampleShadowmapFilteredHighQuality(TEXTURE2D_SHADOW_ARGS(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT), selfShadowCoord, shadowSamplingData);
+    #elif defined(_SHADOWS_SOFT)
+    if (shadowParams.y > SOFT_SHADOW_QUALITY_OFF)
+        attenuation = SampleShadowmapFiltered(TEXTURE2D_SHADOW_ARGS(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT), selfShadowCoord, shadowSamplingData);
+    else
+        attenuation = float(SAMPLE_TEXTURE2D_SHADOW(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT, selfShadowCoord.xyz));
     #else
     attenuation = float(SAMPLE_TEXTURE2D_SHADOW(_GakuSelfShadowMapRT, sampler_GakuSelfShadowMapRT, selfShadowCoord.xyz));
     #endif
 
-    // 카툰 렌더링에서 좀 더 그림자 경계를 깔끔하게 출력하기 위한 Magic Number~
-    attenuation = smoothstep(0.15, 0.3, attenuation);
+    // // 카툰 렌더링에서 좀 더 그림자 경계를 깔끔하게 출력하기 위한 Magic Number~지만 일단 생략
+    // attenuation = smoothstep(0.15, 0.3, attenuation);
     
     // 내적으로 인한 보정
     attenuation *= _GakuSelfShadowUseNdotLFix ? smoothstep(0.1, 0.2, saturate(dot(normalWS, _GakuSelfShadowLightDirection))) : 1;
